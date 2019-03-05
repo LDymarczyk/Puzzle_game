d5b2ed7dd0f432550c329cca66d8f4599aff8c98
adrixop95 <adrixop95@me.com>
2019-03-05 17:10:44 +0100

    some source code

---
+++ src/klocki_plane/Assets/Scripts/Move_Puzzle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Puzzle : MonoBehaviour {
+
+    public  Sprite[]    sprites;
+    public  float       speed       =   1;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        moving();
+    }
+
+    public void moving() {
+        transform.position  -=  new Vector3( speed * Time.deltaTime, 0, 0 );
+    }
+
+    public void goto_start() {
+        int rand    =   Random.Range(0, 3);
+        GetComponent<SpriteRenderer>().sprite = sprites[rand];
+        transform.position  =   new Vector3( 15, 0, 0 );
+    }
+
+}
